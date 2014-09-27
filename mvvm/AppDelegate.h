3ac2bb4b382a386f50d06f5c51da9176475b76a6
Pascal Drouilly <pdrouilly@xebia.fr>
2014-09-27 14:45:59 +0200

    Initial Commit

---
+++ mvvm/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  mvvm
+//
+//  Created by Pascal Drouilly on 27/09/14.
+//  Copyright (c) 2014 PMU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
