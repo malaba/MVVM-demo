98f9b7a4d8a0f5cc9a4973734d62da635ada0916
Pascal Drouilly <pdrouilly@xebia.fr>
2014-09-27 17:41:12 +0200

    Added a Child ViewControlle

---
+++ mvvm/ViewControllers/Child_VC.h
@@ -0,0 +1,13 @@
+//
+//  Child_VC.h
+//  mvvm
+//
+//  Created by Pascal Drouilly on 27/09/14.
+//  Copyright (c) 2014 PMU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Child_VC : UIViewController
+
+@end
---
+++ mvvm/ViewControllers/Child_VC.m
@@ -0,0 +1,20 @@
+//
+//  Child_VC.m
+//  mvvm
+//
+//  Created by Pascal Drouilly on 27/09/14.
+//  Copyright (c) 2014 PMU. All rights reserved.
+//
+
+#import "Child_VC.h"
+#import "Child_VC+pivate.h"
+
+@implementation Child_VC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+@end
